bd5009697ceefa8fcce67af66551c06c56ed5e70
Malnayr <lamryan0815@gmail.com>
2017-04-04 20:05:07 -0600

    Coverted each position on the board as a possible ship

--- Sources/Board.swift
+++ Sources/Board.swift
@@ -1,22 +1,14 @@
 public class Board
 {
   //Empty 2D array
-  var board = [[String]]()
+  var board = [[Ship?]]()
   var vCoords = ["A","B","C","D","E","F","G","H","I","J"]
   var hCoords = ["1","2","3","4","5","6","7","8","9","10"]
 
   public init()
   {
     //Create a 10 x 10 two dimensional array
-    for _ in 0..<10
-    {
-      var rowArray = [String]()
-      for _ in 0..<10
-      {
-        rowArray.append("*")
-      }
-      board.append(rowArray)
-    }
+    self.board = [[Ship?]]
   }
 
   subscript (row : Int, column : Int) -> String
